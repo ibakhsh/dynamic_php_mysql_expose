4064ef6b86bac84f73ef875d7775934d116149b3
Ibrahim Bakhsh <igbmadinah@gmail.com>
2017-05-06 14:15:49 +0300

    branching a test..

--- config/actionMap.php
+++ config/actionMap.php
@@ -1,14 +1,6 @@
 <?php 
 $actionMap = array(
-               array('parkingGet'=>'parking', 'method'=>'GET'), 
-               array('listGet'=>'static_lists', 'method'=>'GET'),
-               array('userGet'=>'user', 'method'=>'POST','unset_columns'=>array('username','password','slash')),
-               array('userPost'=>'user', 'method'=>'POST'),
-               array('categoryGet'=>'categories', 'method'=>'GET'),
-               array('serviceGet'=>'service', 'method'=>'GET'),
                array('testPost'=>'test', 'method'=>'POST'),
-               array('testGet'=>'test', 'method'=>'GET'),
-               array('login'=>'login.php', 'method'=>'POST'),
-               array('register'=>'register.php', 'method'=>'POST')
+               array('testGet'=>'test', 'method'=>'GET')
             );
 ?>
